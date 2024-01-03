3039ef0b0eabff34778681d977f813145db33b51
Jessica Kelly <JessKelly91@outlook.com>
2024-01-03 13:06:02 -0500

    completed machine

--- makeText.js
+++ makeText.js
@@ -1,2 +1,51 @@
 /** Command-line tool to generate Markov text. */
+const fs = require('fs')
+const process = require('process');
+const markov = require('./markov');
+const axios = require('axios');
 
+// make markov machine to generate text from
+function generateText(text){
+    let mm = new markov.MarkovMachine(text);
+    console.log(mm.makeText());
+}
+
+// read file and generate text from it
+function makeTextFile(path){
+    fs.readFile(path, 'utf8', (err, data) => {
+        if(err){
+            console.log(`Error reading ${path}: ${err}`);
+            process.exit(1);
+        }
+        else{
+            generateText(data);
+        }
+    });
+}
+
+// read URL and make text from it
+
+async function makeTextUrl(url){
+    try{
+        let resp = await axios.get(url);
+        generateText(resp.data);
+    }
+    catch(err){
+        console.log(`Error fetching ${url}: ${err}`);
+        process.exit(1);
+    }
+}
+
+let method = process.argv[2];
+let path = process.argv[3];
+
+if (method === "file") {
+    makeTextFile(path);
+}
+else if (method === "url"){
+    makeTextUrl(path);
+}
+else{
+    console.log(`Unknown method" ${method}`);
+    process.exit(1);
+}
--- markov.js
+++ markov.js
@@ -6,8 +6,11 @@ class MarkovMachine {
   /** build markov machine; read in text.*/
 
   constructor(text) {
+    // split text into individual words
     let words = text.split(/[ \r\n]+/);
+    // delete empty strings
     this.words = words.filter(c => c !== "");
+    // create how words are chained together
     this.makeChains();
   }
 
@@ -17,13 +20,45 @@ class MarkovMachine {
    *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
 
   makeChains() {
-    // TODO
+    // create "chains" that only allows machine to choose next index of array
+
+    // create new Map
+    let chains = new Map();
+
+    // iterate through each word in the text entered, and create arrays for potential words to follow each word based on text input
+    for(let i=0; i < this.words.length; i++){
+      let word = this.words[i];
+      let nextWord = this.words[i + 1] || null;
+
+      if (chains.has(word)) chains.get(word).push(nextWord);
+      else chains.set(word, [nextWord]);
+    }
+
+    // assign this to the this.chains property
+    this.chains = chains;
   }
 
 
   /** return random text from chains */
 
+  static choice(arr){
+    return arr[Math.floor(Math.random() * arr.length)];
+  }
+
   makeText(numWords = 100) {
-    // TODO
+    let output = [];
+    let keys = Array.from(this.chains.keys());
+    let key = MarkovMachine.choice(keys); 
+
+    while (output.length < numWords && key !== null) {
+      output.push(key);
+      key = MarkovMachine.choice(this.chains.get(key));
+    }
+    
+    return output.join(" ");
   }
 }
+
+module.exports = {
+  MarkovMachine,
+};
